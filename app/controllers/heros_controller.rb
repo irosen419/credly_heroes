2df68b814863a0c3d7dabdb443e21a0b18274688
irosen419 <irosen419@gmail.com>
2022-09-26 23:50:11 -0500

    add links to hero show page

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -4,15 +4,30 @@ class HerosController < ApplicationController
 
   require "uri"
   require "net/http"
+
+  $baseUrl = 'http://gateway.marvel.com/v1/public/characters'
+  $timestamp = Time.new.sec.to_s
+  $hash = Digest::MD5.hexdigest ($timestamp + ENV["PRIVATE_KEY"] + ENV["PUBLIC_KEY"])
+  $str = "?ts=#{$timestamp}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{$hash}"
+
+
   # Retrieve HEROES from MARVEL API
   def index
-    baseUrl = 'http://gateway.marvel.com/v1/public/characters?'
-    timestamp = Time.new.sec.to_s
-    hash = Digest::MD5.hexdigest (timestamp + ENV["PRIVATE_KEY"] + ENV["PUBLIC_KEY"])
-    str = "ts=#{timestamp}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{hash}"
-    response = RestClient.get(baseUrl + str)
+    response = RestClient.get($baseUrl + $str)
     @heroes = JSON.parse(response)["data"]["results"]
-    # render json: @heroes[0]
+  end
+
+  def show
+    @hero = RestClient.get($baseUrl + "/" + params[:id] + $str)
+    render json: @hero
+  end
+
+  def find_hero
+    hero_name = params["hero"]
+    response = RestClient.get($baseUrl + $str + "%limit=50&name=#{hero_name}")
+    @hero = JSON.parse(response)["data"]["results"][0]
+    redirect_to hero_path(@hero["id"])
+    
   end
 
   # def get_badges
@@ -22,10 +37,15 @@ class HerosController < ApplicationController
   #   https.use_ssl = true
     
   #   request = Net::HTTP::Get.new(url)
-  #   form_data = [['email', 'irosen419@gmail.com'],['password', 'Python419!']]
+  #   form_data = [['email', 'irosen419$gmail.com'],['password', 'Python419!']]
   #   request.set_form form_data, 'multipart/form-data'
   #   response = https.request(request)
   #   puts response.read_body
   # end
 
+  private
+
+  def search_params
+    params.require(:hero).permit(:id, :name, :thumbnail)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/heros', to: "heros#index"
+  resources :heros, only: [:index, :show]
+  post '/find_hero', to: "heros#find_hero"
   
   get '/badge_templates', to: "badges#get_badge_templates"
   post '/issue_badge', to: "badges#issue_badge"
