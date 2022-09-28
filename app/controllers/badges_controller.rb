d93ca946b5fbb9bfa6392aed58b3f2a3578c40ec
irosen419 <irosen419@gmail.com>
2022-09-28 11:01:16 -0500

    prep for heroku

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -4,6 +4,6 @@ class BadgesController < ApplicationController
   def issue_badge
     first_name, last_name = params[:hero_name].split(" ")
     post_badge("#{params[:hero_id]}@gmail.com", first_name, last_name, params[:badge_template_activities][0][:id], Time.new.to_s)
-    redirect_to hero_path(params[:hero_id])
+    # redirect_to hero_path(params[:hero_id])
   end
 end
